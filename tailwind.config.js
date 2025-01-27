7c276af50c9f0414c4b51957b4a6b9375c49a74a
samanta subedi <samantasubedi333@gmail.com>
2025-01-27 23:06:35 +0545

    portfolio completed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontsize: {
+        xxs: "10px",
+      },
+    },
+  },
+  plugins: [],
+};
